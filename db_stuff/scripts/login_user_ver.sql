1f7991fbcb79758302a8b5d35b4f887eeb5460a1
Rodrigo Arguello Serrano <rodrigo19662001@gmail.com>
2024-03-11 19:12:37 -0700

    Final scripts

--- db_stuff/scripts/login_user_ver.sql
+++ db_stuff/scripts/login_user_ver.sql
@@ -5,7 +5,7 @@
 -- from roles
 -- right join users on users.id = roles.uer_id;
 
-select roles.role_description
+select roles.role_description, users.id
 from users
 join roles on users.id = roles.user_id
 where users.user_login = 'admin1' and users.user_login_pin = '12345';
--- db_stuff/scripts/updating_customer.sql
+++ db_stuff/scripts/updating_customer.sql
@@ -3,10 +3,10 @@ SELECT * from roles;
 Select * from accounts;
 
 -- users
--- UPDATE users
--- SET user_login_pin = '67891', user_login='rod1996', holder='Rodrigo'
--- WHERE id = 3;
+UPDATE users
+SET user_login_pin = '67891', user_login='rod1996', holder='Rodrigo'
+WHERE id = 3;
 -- account
--- UPDATE accounts
--- SET balance=625.00, active = 0
--- WHERE user_id = 3;
+UPDATE accounts
+SET  active = 0
+WHERE user_id = 3;
